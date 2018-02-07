4d5ad39a7d2ddd3ca8dac739a26f6371ced1ca05
baligaandris <baligaandris@gmail.com>
2018-02-07 16:57:00 +0000

    Initial Commit

---
+++ Assets/BulletStandardBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletStandardBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(GetComponent<Rigidbody2D>().velocity);
+        Destroy(gameObject);
+    }
+}
