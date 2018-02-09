13231ea17a3e55181a281dbdc198e253c34dc1dc
cbevan200 <CBEVAN200@caledonian.ac.uk>
2018-02-09 11:10:30 +0000

    Add files via upload

---
+++ MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+	public void LoadMainLevel()
+	{
+		Application.LoadLevel ("Scene1");
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit ();
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
