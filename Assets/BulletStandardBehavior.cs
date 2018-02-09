847240f7a80df7331f3b85ecc69794ee224b9765
baligaandris <baligaandris@gmail.com>
2018-02-09 16:34:37 +0000

    implemented changes
    added 2 player controller support
    set bullets to disappear after a set distance

--- Assets/BulletStandardBehavior.cs
+++ Assets/BulletStandardBehavior.cs
@@ -3,20 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletStandardBehavior : MonoBehaviour {
-
+    float bulletDistance = 8.0f;
+    public float bulletRange;
+    private Vector3 startingPos;
 	// Use this for initialization
 	void Start () {
-		
+        startingPos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Vector3.Distance(startingPos,transform.position) > bulletRange)
+        {
+            Destroy(gameObject);
+        }
 	}
+    //void BulletRange()
+    //{
+    //    bulletRange = bulletDistance - 3.0f;
+    //   /* if (bulletDistance > bulletRange)
+    //    {
+    //        Destroy(gameObject);
+    //    */
+    //}
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(GetComponent<Rigidbody2D>().velocity);
+        if (collision.gameObject.GetComponent<Rigidbody2D>() != null)
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(GetComponent<Rigidbody2D>().velocity* ((bulletRange - Vector3.Distance(startingPos, transform.position))/bulletRange));
+        }
         Destroy(gameObject);
     }
 }
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -10,32 +10,89 @@ public class PlayerMovement : MonoBehaviour {
     public GameObject bullet;
     public GameObject bullet2;
     public float fireSpeed;
+    private bool canFire1 = true;
+    private bool canFire2 = true;
 
-	// Use this for initialization
-	void Start () {
+    public float deadzone = 0.25f;
+
+    //axes to use
+    private string h;
+    private string v;
+    private string hAim;
+    private string vAim;
+    private string f1;
+    private string f2;
+
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody2D>();
+        if (gameObject.tag=="Player")
+        {
+            h = "Horizontal";
+            v = "Vertical";
+            hAim = "HorizontalAim";
+            vAim = "VerticalAim";
+            f1 = "Fire1";
+            f2 = "Fire2";
+        }
+        if (gameObject.tag == "Player2")
+        {
+            h = "Horizontal2";
+            v = "Vertical2";
+            hAim = "HorizontalAim2";
+            vAim = "VerticalAim2";
+            f1 = "Fire12";
+            f2 = "Fire22";
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float xAxis = Input.GetAxis("Horizontal");
-        float yAxis = Input.GetAxis("Vertical");
+        float xAxis = -Input.GetAxis(h);
+        float yAxis = -Input.GetAxis(v);
         Accellerate(xAxis, yAxis);
 
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetAxis(f1)!=0)
         {
-            fireVector = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
-            GameObject newBullet = Instantiate(bullet, transform.position, Quaternion.identity);
-            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), newBullet.GetComponent<Collider2D>());
-            newBullet.GetComponent<Rigidbody2D>().AddForce(fireVector*fireSpeed);
+            if (canFire1)
+            {
+                //fireVector = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+                if (Input.GetAxis(hAim)!= 0 || Input.GetAxis(vAim)!=0)
+                {
+                    fireVector = new Vector2(Input.GetAxis(hAim), -Input.GetAxis(vAim));
+                    print(fireVector);
+                    GameObject newBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+                    Physics2D.IgnoreCollision(GetComponent<Collider2D>(), newBullet.GetComponent<Collider2D>());
+                    newBullet.GetComponent<Rigidbody2D>().AddForce(fireVector * fireSpeed);
+                    canFire1 = false;
+                }
+            }
         }
-        if (Input.GetButtonDown("Fire2"))
+        else
         {
-            fireVector = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
-            GameObject newBullet = Instantiate(bullet2, transform.position, Quaternion.identity);
-            newBullet.GetComponent<BulletSwitchBehavior>().SetWhoFiredMe(gameObject);
-            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), newBullet.GetComponent<Collider2D>());
-            newBullet.GetComponent<Rigidbody2D>().AddForce(fireVector * fireSpeed);
+            canFire1 = true;
+        }
+        if (Input.GetAxis(f2)!=0)
+        {
+            if (canFire2)
+            {
+                //fireVector = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+                if (Input.GetAxis(hAim) != 0 || Input.GetAxis(vAim) != 0)
+                {
+                    fireVector = new Vector2(Input.GetAxis(hAim), -Input.GetAxis(vAim));
+                    print(fireVector);
+                    GameObject newBullet = Instantiate(bullet2, transform.position, Quaternion.identity);
+                    newBullet.GetComponent<BulletSwitchBehavior>().SetWhoFiredMe(gameObject);
+                    Physics2D.IgnoreCollision(GetComponent<Collider2D>(), newBullet.GetComponent<Collider2D>());
+                    newBullet.GetComponent<Rigidbody2D>().AddForce(fireVector * fireSpeed);
+                    canFire2 = false;
+                }
+            }
+
+        }
+        else
+        {
+            canFire2 = true;
         }
 
     }
@@ -43,9 +100,13 @@ public class PlayerMovement : MonoBehaviour {
     void Accellerate(float y, float x)
     {
         Vector2 force = new Vector2(0, 0);
+        if (new Vector2 (x,y).magnitude > deadzone)
+        {
+            force = (transform.up * y * accelleration) + (transform.right * x * accelleration);
+        }
         if (y != 0 || x != 0)
         {
-            force = (transform.up * -y * accelleration) + (transform.right * x * accelleration);
+            
 
         }
         rb.AddForce(force);
