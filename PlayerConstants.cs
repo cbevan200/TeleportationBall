38bfd4e33b3b178e7f7356e5464e7e330c9fdb1b
cbevan200 <CBEVAN200@caledonian.ac.uk>
2018-02-10 14:24:13 +0000

    Add files via upload
    This scene has collab activated and the player constant script will store the player score

---
+++ PlayerConstants.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerConstants : MonoBehaviour {
+    int player1Score;
+    string player1ScoreText;
+    int player2Score;
+    string player2ScoreText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
