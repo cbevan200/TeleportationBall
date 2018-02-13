577f0bbd4b1659bbdeaedd36b12abf05bf608496
baligaandris <baligaandris@gmail.com>
2018-02-13 16:59:32 +0000

    Robert's updates to scoring system

---
+++ Assets/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    Vector2 defaultPos;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        defaultPos = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        {
+
+            transform.position = defaultPos;
+        }
+
+    }
+}
